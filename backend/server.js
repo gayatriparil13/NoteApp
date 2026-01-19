b1aa87e890c17ea4872703d132a3818c3a727f64
Gayatri Patil <145528516+gayatriparil13@users.noreply.github.com>
2026-01-19 15:20:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+
 // Routes
 app.use("/api/notes", notesRoutes);
 
@@ -20,3 +25,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
